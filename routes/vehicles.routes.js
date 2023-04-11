2abfa88e505fb5bac72e25519190dfef8aa830d2
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-04-10 19:12:29 -0500

    route

--- routes/vehicles.routes.js
+++ routes/vehicles.routes.js
@@ -7,5 +7,6 @@ router.get('/:id', vehiclesController.getVehicle);
 router.post('/', vehiclesController.createVehicle);
 router.delete('/:id', vehiclesController.deleteVehicle);
 router.put('/:id', vehiclesController.replaceVehicle);
+router.patch('/:id', vehiclesController.updateVehicle);
 
 module.exports = router;
