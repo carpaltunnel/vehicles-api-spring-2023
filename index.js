90e1c596fa6aee82b1693459706ff44c7b6dc816
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-05-01 19:02:05 -0500

    initial mongo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const vehiclesRouter = require('./routes/vehicles.routes');
 const paintMiddleware = require('./middleware/supportedPaint');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
+const db = require('./lib/database');
 
 const port = 3000;
 const app = express();
@@ -17,6 +18,19 @@ app.use('/api/v1/vehicles', vehiclesRouter);
 
 // Errorhandlers always go last
 app.use(errorHandlerMiddleware());
+
+const dbConfig = {
+  hostname: '127.0.0.1',
+  port: 27017,
+  database: 'arca',
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    maxPoolSize: 3,
+  }
+};
+
+db.configure(dbConfig);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
